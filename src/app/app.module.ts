7e0a815a7a4cb643ac9c7814e1d73e081fd7a92f
shehan.kalharaj@gmail.com <shehan.kalharaj@gmail.com>
2022-05-05 20:31:34 +0530

    https://github.com/jayathunga/CRM-Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { CustomerComponent } from './customer/customer.component';
+import { TaskComponent } from './task/task.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProductsComponent } from './products/products.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    CustomerComponent,
+    TaskComponent,
+    EmployeeComponent,
+    ProductsComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
